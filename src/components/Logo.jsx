e456a0c72ec68817f062c11ae51f4d2ce56dd26c
Marlobrado <fernandogynbsb@gmail.com>
2025-12-09 16:33:15 -0300

    chore: add git hooks

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -12,7 +12,7 @@ const Logo = () => {
         className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
         whileHover={{
           backgroundColor: ['#121212', 'rgba(131,58,180,1)', 'rgba(253,29,29,1)', 'rgba(252,176,69,1)', 'rgba(131,58,180,1)', '#121212'],
-          transition: { duration: 1, repeat: Infinity },
+            transition: { duration: 1, repeat: Infinity },
         }}
       >
         FH
