4722d7d1e8df35610d3c886058219560b3400923
Marlobrado <fernandogynbsb@gmail.com>
2025-12-18 17:28:32 -0300

    feat: projects paje completed

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -10,8 +10,9 @@ import project1 from '../../public/images/projects/crypto-screener-cover-image.j
 
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl">
-      <Link className="w-1/2 cursor-pointer overflow-auto rounded-xl" href={link} target="_blank">
+    <article className="w-full flex items-center justify-between relative rounded-br-2xl rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+      <Link className="w-1/2 cursor-pointer overflow-hidden rounded-lg" href={link} target="_blank">
         <Image src={img} alt={title} className="w-full h-auto" />
       </Link>
 
@@ -39,6 +40,35 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   );
 };
 
+const Project = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-2xl flex-col border border-solid border-dark bg-light p-6 relative shadow-2xl">
+      <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+
+      <Link className="w-full cursor-pointer overflow-hidden rounded-lg" href={link} target="_blank">
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link href={link} target="_blank" className="relative group">
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+          <span className="h-[4px] inline-block bg-dark absolute left-0 -bottom-[-0.5rem] group-hover:w-full transition-[width] ease duration-500 w-0"></span>
+        </Link>
+        {/* <p className="my-2 font-medium text-dark">{summary}</p> */}
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link href={link} target="_blank" className=" text-lg font font-semibold underline">
+            Visit
+          </Link>
+          <Link className="w-8" href={github} target="_blank">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
 const projects = () => {
   return (
     <>
@@ -50,7 +80,7 @@ const projects = () => {
       <main className="w-full  mb-16 flex flex-col items-center justify-center">
         <Layout className=" pt-16">
           <AnimatedText className="mb-16" text={userData.animatedTextProjects} />
-          <div className="grid grid-cols-12 gap-24">
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
             <div className="col-span-12">
               <FeaturedProject
                 className="col-span-12"
@@ -63,11 +93,58 @@ const projects = () => {
               />
             </div>
 
-            <div className="col-span-6">project 1</div>
-            <div className="col-span-6">project 2</div>
-            <div className="col-span-12">Featured Projects</div>
-            <div className="col-span-6">project 3</div>
-            <div className="col-span-6">project 4</div>
+            <div className="col-span-6">
+              <Project
+                type={'Project Type'}
+                title={'Project Title'}
+                // summary={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi.'}
+                img={project1}
+                link={'/'}
+                github={'/'}
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                type={'Project Type'}
+                title={'Project Title'}
+                // summary={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi.'}
+                img={project1}
+                link={'/'}
+                github={'/'}
+              />
+            </div>
+
+            <div className="col-span-12">
+              <FeaturedProject
+                className="col-span-12"
+                type={'Crypto Screener Application'}
+                title={'Meu titulaço'}
+                summary={'a feature=rich crypto screener app using React, tailwindcss, context api, react-router-dom an Recjarts.'}
+                img={project1}
+                link={'/'}
+                github={'/'}
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                type={'Project Type'}
+                title={'Project Title'}
+                // summary={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi.'}
+                img={project1}
+                link={'/'}
+                github={'/'}
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                type={'Project Type'}
+                title={'Project Title'}
+                // summary={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi.'}
+                img={project1}
+                link={'/'}
+                github={'/'}
+              />
+            </div>
           </div>
         </Layout>
       </main>
