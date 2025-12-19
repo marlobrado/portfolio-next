7b3b834e13fdc70b78805c577969ed421aa1e2a5
Marlobrado <fernandogynbsb@gmail.com>
2025-12-19 14:36:04 -0300

    style: added dark moto to article pages

--- src/pages/articles.jsx
+++ src/pages/articles.jsx
@@ -48,27 +48,27 @@ const Article = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
       viewport={{ once: true }}
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark border border-solid border-dark first:mt-0 border-r-4 border-b-4"
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light dark:bg-dark text-dark dark:text-light border border-solid border-dark dark:border-light first:mt-0 border-r-4 border-b-4"
     >
       <MovingImg img={img} title={title} link={link} />
-      <span className="text-primary font-semibold pç-4">{date}</span>
+      <span className="text-primary dark:text-primaryDark font-semibold pl-4">{date}</span>
     </motion.li>
   );
 };
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
-      <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+    <li className="relative col-span-1 w-full p-4 bg-light dark:bg-dark border border-solid border-dark dark:border-light rounded-2xl dark:text-light">
+      <div className="absolute top-0 -right-4 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl" />
 
-      <Link className="w-full inline-block cursor-pointer overflow-hidden ronded-lg" href={link} target="_blank">
+      <Link className="w-full inline-block cursor-pointer overflow-hidden rounded-lg" href={link} target="_blank">
         <FramerImage src={img} alt={title} className="w-full h-auto" whileHover={{ scale: 1.05 }} transition={{ duration: 1 }} />
       </Link>
       <Link href={link} target="_blank">
         <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">{title}</h2>
       </Link>
       <p className="text-sm mb-2">{summary}</p>
-      <span className="text-primary font-semibold">{time}</span>
+      <span className="text-primary dark:text-primaryDark font-semibold">{time}</span>
     </li>
   );
 };
@@ -100,7 +100,7 @@ const articles = () => {
               link={'/'}
             />
           </ul>
-          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32 ">All Articles</h2>
+          <h2 className="font-bold text-4xl w-full text-center my-16 mt-32 dark:text-light">All Articles</h2>
           <ul>
             <Article img={article1} title="Build A Custom Pagination Component In Reactjs From Scratch." date="March 22, 2023" link={'/'} />
             <Article img={article2} title="Build A Custom Pagination Component In Reactjs From Scratch." date="March 22, 2023" link={'/'} />
